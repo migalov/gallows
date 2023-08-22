52e2f120ca1357d3659acf12b8e88b8683e17337
Michel Muniagiseny <mafri.michel@gmail.com>
2023-08-22 20:15:03 +0300

    CLW-2 Changes

--- main.js
+++ main.js
@@ -1,81 +1,109 @@
+
+
 /* HTML-теги */
-const template = document.querySelector("#empty-cell");
+const templateCell = document.querySelector("#empty-cell");
 
-
+/* Classes */
+const mainClass = 'gallows';
+const groupCellsClass = 'gallows-group-inner';
+const cellClass = "gallows-cell";
 
 /* Счетчики игры */
 let statusGame = {
   countFalis: 0,
   maxCountFalis: 7,
-  countGuessLetter: 0,
-  question: "",
-  randomWord: "Something"
+  countGuessLetter: 0
+}
+
+/* Красим поля в красный цвет */
+const paintTheInputs = () => {
+  document.querySelector(`.${groupCellsClass}`).classList.add(`${groupCellsClass}--lose`);
 }
 
 /* Проверка состояния игры */
-const checkStateGame = () => {
-  if(statusGame.countFalis < statusGame.maxCountFalis) {
-    console.log("Let's continue");
+const checkStateGame = (word) => {
+  if(statusGame.countFalis >= statusGame.maxCountFalis) {
+    document.querySelectorAll(`.${cellClass}`).forEach(_e => {
+      _e.disabled = true;
+    });
+    paintTheInputs();
+    endDialogue(false);
   }
-  else {
-    console.log("You lose");
-    return;
+  if(statusGame.countGuessLetter === word.length) {
+    endDialogue(true);
   }
-  if(statusGame.countGuessLetter === statusGame.randomWord.length) {
-    console.log("You win");
-  }
-  console.log(statusGame);
 }
 
 /* Генерируем поля для вставки букв */
-const generateGame = (windowClass, word, cellsClass) => {
-
+const generateGame = (_groupCellsClass, _cellsClass, word) => {
+  
   for(let i = 0; i < word.length; i++) {
 
-    let clone = template.content.cloneNode(true),
-        cell = clone.querySelector(cellsClass);
+    let clone = templateCell.content.cloneNode(true),
+        cell = clone.querySelector(`.${_cellsClass}`);
         cell.dataset.index = i;
-
-        document.querySelector(windowClass).appendChild(cell);
-        document.querySelector(windowClass).style = `grid-template-columns: repeat(${word.length}, 1fr)`
-
+        
+        document.querySelector(`.${_groupCellsClass}`).appendChild(cell);
+        document.querySelector(`.${_groupCellsClass}`).style = `grid-template-columns: repeat(${word.length}, 1fr)`
   }
+  document.querySelector(`.${_groupCellsClass}`).style.height = `${document.querySelector(`.${_cellsClass}`).offsetWidth / 2}px`;
 }
 
 /* Проверяем букву и вставляем */
-const checkAndInsertLetter = (word, selector) => {
-  document.querySelectorAll(selector).forEach(el => {
+const checkAndInsertLetter = (word, _cellsClass) => {
+  document.querySelectorAll(`.${_cellsClass}`).forEach(el => {
     el.addEventListener("keypress", (_e) => {
-      if (_e.target.dataset.index == word.indexOf(_e.key)) {
+      
+      if (_e.target.dataset.index == word.indexOf(_e.key, _e.target.dataset.index)) {
         _e.target.value = _e.key;
         _e.target.disabled = true;
+        _e.target.classList.add(`${_cellsClass}--success`);
+        _e.target.nextElementSibling?.focus();
         statusGame.countGuessLetter = statusGame.countGuessLetter + 1;
-        console.log("Yes");
       }
       else {
         _e.preventDefault();
         statusGame.countFalis = statusGame.countFalis + 1;
         console.log("No");
-        _e.target.classList.add("gallows-cell--error");
+        _e.target.classList.add(`${_cellsClass}--error`);
         setTimeout(() => {
-          _e.target.classList.remove("gallows-cell--error");
+          _e.target.classList.remove(`${_cellsClass}--error`);
         }, 500)
+        document.querySelectorAll(`[data-path="${statusGame.countFalis}"]`).forEach(__e => {
+          __e.style.opacity = "1";
+        })
       }
-      checkStateGame();
+      checkStateGame(word);
     });
-    el.addEventListener("rezise", (_e) => {
-      console.log(_e.target.outerWidth);
-    })
-
   });
 }
 
-const initGame = (windowClass, cellsClass, word) => {
-//   let username = prompt("Hi! Let's game to Gallows. What it's your name?");
-//   let start = confirm(`Nice to meet you${username ? ", "+username : ""}! Nice to meet you, Misha. The rules of the game are simple. You must guess the word. You have 7 tries. If you spend all attempts before you give up the word, you lose.
-// Good luck.`);
-    generateGame(windowClass, word, cellsClass);
-    checkAndInsertLetter(word, cellsClass);
+/* Диалог с игроком после конца игры */
+const endDialogue = (status) => {
+  status ? `win` : `lose`;
+  let stay = confirm(`You are ${status}! Do you want to start over?`);
+    if (stay) {
+      document.location.reload();
+    }
+    else {
+      let exit = confirm("Do you want to finish the game?");
+      if (exit) {
+        window.close();
+      }
+    }
+    return;
 }
 
-initGame('.gallows-group-inner', '.gallows-cell', statusGame.randomWord);
+
+const initGame = (_groupCellsClass, _cellsClass, word) => {
+    generateGame(_groupCellsClass, _cellsClass, word);
+    checkAndInsertLetter(word, _cellsClass);
+}
+
+const data = await fetch('https://api.npoint.io/e0e7d92aa4c214f6d26c')
+  .then((response) => response.json())
+  .then((json) => {
+    let randomElement = json[Math.floor(Math.random() * json.length)];
+    document.querySelector(`.${mainClass}__question`).innerHTML = randomElement.question;
+    initGame(groupCellsClass, cellClass, randomElement.answer.toLowerCase());
+  });
